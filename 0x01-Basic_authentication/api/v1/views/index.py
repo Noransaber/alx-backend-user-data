8d817fc7930f4a93c558f58079c2566359fbe7cd
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-12 12:21:27 +0200

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> None:
       - Unauthorized error.
       """
       return abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+      """GET /api/v1/forbidden
+      Return:
+      - forbidden error.
+      """
+      return abort(403)
