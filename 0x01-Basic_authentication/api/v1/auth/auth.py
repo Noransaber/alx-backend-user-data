d73dae171265507d3ad1d9ac9135c150b9aee0ae
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-12 13:50:36 +0200

    Update auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+import request from flask
+
+class Auth:
+  """
+  3. Auth class
+  """
+  def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    """
+    4. require_auth function"""
+    return False
+
+  def authorization_header(self, request=None) -> str:
+    """
+    5. authorization_header function"""
+    return None
+  
+  def current_user(self, request=None) -> TypeVar('User'):
+    """
+    6. current_user function"""
+    return None
