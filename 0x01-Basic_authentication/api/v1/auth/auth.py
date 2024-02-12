5dbb99ffd1f711aec950a20c8a892acb723de595
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-12 16:28:23 +0200

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,19 @@ class Auth:
   def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
     """
     4. require_auth function"""
-    return False
+    # Check if the exculded_paths list is empty, or None
+    if not excluded_paths or excluded_paths is None:
+      return True
+
+    # Check if the path is none or has a value
+    if path is None:
+      return True
+
+    # Check if the path is in the exculded_paths list
+    for excluded_path in excluded_paths:
+      if fnmatch.fnmatch(path, excluded_path):
+        return False
+    return True
 
   def authorization_header(self, request=None) -> str:
     """
@@ -23,3 +35,5 @@ class Auth:
     """
     6. current_user function"""
     return None
+
+  
