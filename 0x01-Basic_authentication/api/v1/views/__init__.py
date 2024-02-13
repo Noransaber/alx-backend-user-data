0db14b50205dcb8f5bbc111ab2f7ce88681c6e30
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-13 13:47:28 +0200

    Update __init__.py

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -5,12 +5,7 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import index_app_views
-from api.v1.views.users import users_app_views
-
-# Import views after the Blueprint creation to avoid circular import issues
-from api.v1.views import views
-
-
+from api.v1.views.index import *
+from api.v1.views.users import *
 
 User.load_from_file()
