4f680e851f6c0d373b0c340422c291cea1428065
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-13 13:47:07 +0200

    Update users.py

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from flask import jsonify, request
 from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
 
 
