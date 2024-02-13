8f0959fa55bb924675a49064abca3df9da247e52
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-13 06:30:11 +0200

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,47 +1,37 @@
 #!/usr/bin/env python3
-""" 3. Auth class
+"""Authentication module.
 """
-
-from flask import Flask, request
+from flask import request
 from typing import List, TypeVar
 import fnmatch
 
 
 class Auth:
+    """Authentication class.
     """
-    3. Auth class
-    """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to check if auth is required.
         """
-        4. require_auth function"""
-        # Check if the path is None
         if path is None:
             return True
 
-        # Check if the excluded_paths list is None or empty
-        if not excluded_paths:
+        if excluded_paths is None or not excluded_paths:
             return True
 
-        # Check if the path is in the excluded_paths list
         for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path.rstrip('/'), excluded_path.rstrip('/')):
+            if fnmatch.fnmatch(path, excluded_path):
                 return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
+        """ Method to get authorization header.
         """
-        5. authorization_header function"""
-        # If request is None, returns None
-        if request is None:
-            return None
-        # If request doesn’t contain the header key Authorization, returns None
-        if request.headers.get('Authorization') is None:
-            return None
-        # return the value of the header request Authorization
-        return request.headers.get('Authorization')
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get user from request.
         """
-        6. current_user function"""
         return None
