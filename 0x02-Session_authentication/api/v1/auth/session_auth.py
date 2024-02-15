a48bd12d969f26b2269789f2868661ee09ed84e5
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-15 06:20:21 +0200

    Create session_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+  #!/usr/bin/env python3
+  """ Empty session
+  """
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """Session Authentication Class"""
+  pass
